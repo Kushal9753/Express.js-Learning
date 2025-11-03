a6b62c51e147377f34afa1d0814a5fde5cee251e
Kushal9753 <kushalsahu352@gmail.com>
2025-11-03 19:19:33 +0530

    feat: add Express.js server with basic routes for home, users, and a delayed response

---
+++ index8.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import morgan from 'morgan'
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get("/", (req, resp)=>{
+  resp.send("Home page")
+})
+
+app.get("/users", (req, resp)=>{
+  resp.send("Users page")
+})
+
+app.get("/wait", (req, resp)=>{
+  setTimeout(()=>{
+    
+  resp.send("result after one sec")
+  }, 2000)
+})
+
+
+app.listen(3200)
