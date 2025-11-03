5eb8c91ced9ae3b065e2bd3e4563a6d4f06b1f00
Kushal9753 <kushalsahu352@gmail.com>
2025-11-03 21:12:53 +0530

    show form data in ejs

---
+++ index11.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express();
+
+app.use(express.urlencoded({extended:false}))
+app.set('view engine', 'ejs')
+
+
+app.get("/add-user", (req, resp)=>{
+  resp.render('addUser')
+})
+
+app.post("/submit-user", (req, resp)=>{
+    resp.render('submitUser', req.body)
+})
+
+
+app.listen(3200)
