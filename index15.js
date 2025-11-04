bf7d7a9243a12a5aa77473533c4cdac63fa40e7d
Kushal9753 <kushalsahu352@gmail.com>
2025-11-04 16:40:32 +0530

    implement student deletion and update functionality with new routes and views

--- index15.js
+++ index15.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express();
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 const dbName = 'school'
 const url = "mongodb://localhost:27017"
 const client = new MongoClient(url)
@@ -62,6 +62,81 @@ client.connect().then((connection)=>{
       const result = await collection.insertOne(req.body)
       resp.send({message:"data stored", success:true, result:result})
    })
+
+
+
+app.delete("/delete/:id",async (req, resp)=>{
+   console.log(req.params.id);
+   const collection = db.collection("students")
+   const result = await collection.deleteOne({_id:new ObjectId(req.params.id)})
+
+   if(result){
+      resp.send({
+         message:"student data deleted",
+         success:true
+      })
+   }else{
+      resp.send({
+         message:"student data not deleted try after some time",
+         success:false
+      })
+   }
+   
+
+})
+
+
+
+app.get("/ui/delete/:id",async (req, resp)=>{
+   console.log(req.params.id);
+   const collection = db.collection("students")
+   const result = await collection.deleteOne({_id:new ObjectId(req.params.id)})
+
+   if(result){
+      resp.send("<h1>student record deleted</h1>")
+   }else{
+           resp.send("<h1>student record not deleted</h1>")
+
+   }
+   
+
+})
+
+
+
+
+
+app.get("/ui/student/:id",async (req, resp)=>{
+   
+   const id = req.params.id
+   console.log(id);
+   const collection = db.collection("students")
+   const result = await collection.findOne({_id:new ObjectId(req.params.id)})  
+    resp.render('update-student',{result})
+
+})
+
+
+
+
+app.get("/student/:id",async (req, resp)=>{
+   
+   const id = req.params.id
+   console.log(id);
+   const collection = db.collection("students")
+   const result = await collection.findOne({_id:new ObjectId(req.params.id)})  
+    resp.send({
+      message:'data fetched',
+      success:true,
+      result:result
+
+    })
+
+})
+
+
+
+
 })
 
 
