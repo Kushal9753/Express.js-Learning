b21920615ceb8da9ad2ca7359e5e553ca024c010
Kushal9753 <kushalsahu352@gmail.com>
2025-11-04 13:05:06 +0530

    data show on web page

--- index15.js
+++ index15.js
@@ -8,17 +8,21 @@ const url = "mongodb://localhost:27017"
 
 const client = new MongoClient(url)
 
-async function dbConnection(){
+app.set("view engine", 'ejs')
+
+
+
+app.get("/", async(req, resp)=>{
    await client.connect()
    const db = client.db(dbName)
    const collection = db.collection('students')
 
-   const result = await collection.find().toArray()
-   console.log(result);
-   
-}
+   const students = await collection.find().toArray()
+   console.log(students);
 
-dbConnection()
+
+   resp.render('students',{students})
+})
 
 
 app.listen(3200)
